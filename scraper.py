5cb93126b526d1d2fbb0b56d3c542281727c03e0
SeanDuffie <duffiesean@gmail.com>
2024-08-19 17:11:06 -0400

    Moved to using xpath instead of class tags

--- scraper.py
+++ scraper.py
@@ -12,6 +12,16 @@ from bs4 import BeautifulSoup, ResultSet, Tag
 
 RTDIR = os.path.dirname(__file__)
 
+class Hero:
+    role = ""
+    name = ""
+    abilities = {}
+
+class Patch:
+    name = ""
+    date = ""
+    heroes: List[Hero] = []
+
 def scrape_website(url: str) -> BeautifulSoup:
     """ Scrape the html document from the website
 
@@ -35,84 +45,6 @@ def scrape_website(url: str) -> BeautifulSoup:
     print('Failed to retrieve HTML.')
     return None
 
-def parse_html(soup: BeautifulSoup) -> pd.DataFrame:
-    """ Extracts useful information from the parsed HTML.
-
-    Args:
-        html (BeautifulSoup): HTML file that has been received and parsed by BeautifulSoup
-
-    Returns:
-        pd.DataFrame: DataFrame that was constructed and formatted from html data
-    """
-    # Find all links that contain an href
-    links = soup.find_all('script')
-    # print(links)
-
-    # For each link, check whether it contains a Wordle option
-    option_list = []
-    link: Tag
-    for link in links:
-        if '\"ingredients\"' in link.text:
-            # print(f'{i}) {link}')
-            # if "/unscramble/" in link['href']:
-            option_list.append(link.text.lower())
-        # option_list.append(link.text)
-
-    options = pd.DataFrame(option_list)
-    return options[0]
-
-def parse_html2(soup: BeautifulSoup, tag: str) -> pd.DataFrame:
-    """ Extracts useful information from the parsed HTML.
-
-    Args:
-        html (BeautifulSoup): HTML file that has been received and parsed by BeautifulSoup
-
-    Returns:
-        pd.DataFrame: DataFrame that was constructed and formatted from html data
-    """
-    ingredients = pd.DataFrame(columns=['Amount', 'Unit', 'Name'])
-    spans: ResultSet = soup.find_all(tag)
-
-    quant: List[float] = []
-    unit: List[str] = []
-    name: List[str] = []
-
-    elem: Tag
-    c = 0
-    row = ['', '', '']
-    for elem in spans:
-        # for i, key in enumerate(['data-ingredient-quantity', 'data-ingredient-unit', 'data-ingredient-name']):
-        if len(elem.attrs) > 0:
-            for res in elem.attrs.keys():
-                if res == 'class':
-                    clss = elem.attrs.get(res)
-
-                    if 'amount' in clss[0]:
-                        quant.append(elem.text)
-                        row[0] = elem.text
-                    elif 'unit' in clss[0]:
-                        unit.append(elem.text)
-                        row[1] = elem.text
-                    elif 'name' in clss[0]:
-                        name.append(elem.text)
-                        row[2] = elem.text
-                        ingredients.loc[c] = row
-                        row = ['', '', '']
-                        c += 1
-                if 'ingredient' in res:
-                    if 'quantity' in res:
-                        quant.append(elem.text)
-                        row[0] = elem.text
-                    elif 'unit' in res:
-                        unit.append(elem.text)
-                        row[1] = elem.text
-                    elif 'name' in res:
-                        name.append(elem.text)
-                        row[2] = elem.text
-                        ingredients.loc[c] = row
-                        c += 1
-
-    return ingredients
 
 def parse_html(soup: BeautifulSoup, tag: str) -> pd.DataFrame:
     """ Extracts useful information from the parsed HTML.
@@ -123,14 +55,6 @@ def parse_html(soup: BeautifulSoup, tag: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: DataFrame that was constructed and formatted from html data
     """
-    patches = [
-        {
-            "name": "",
-            "date": "",
-            
-        }
-    ]
-    
     spans: ResultSet = soup.find_all(tag)
     for elem in spans:
         # for i, key in enumerate(['data-ingredient-quantity', 'data-ingredient-unit', 'data-ingredient-name']):
@@ -139,7 +63,7 @@ def parse_html(soup: BeautifulSoup, tag: str) -> pd.DataFrame:
                 if res == 'class':
                     clss = elem.attrs.get(res)
                     val = elem.text
-                    if clss == "PatchNotes-patchTitle"
+                    print(f"{clss} | {val}")
     # https://overwatch.blizzard.com/en-us/news/patch-notes/
     # body
         # main
@@ -150,52 +74,96 @@ def parse_html(soup: BeautifulSoup, tag: str) -> pd.DataFrame:
                         # div class="PatchNotes-path PatchNotes-live"
                         # NOTE: This section contains the important info on each patch
                             # h3 class="PatchNotes-patchTitle"
-    title = "Overwatch 2 Retail Patch Notes - July 12, 2024".split(" - ")
-    name = title[0]
-    date = datetime.datetime.strptime(title[1], "%B %d, %Y")
+                    if clss == "PatchNotes-patchTitle":
+                        # title = "Overwatch 2 Retail Patch Notes - July 12, 2024".split(" - ")
+                        title = val
+                        name = title[0]
+                        date = datetime.datetime.strptime(title[1], "%B %d, %Y")
                             # div class="PatchNotes-section PatchNotes-section-hero_update"
                                 # h4 class="PatchNotes-sectionTitle"
-    role = "Tank"
-    print(name, date, role)
+                        print(f"Running patch {name} from {date}")
+                    elif clss == "PatchNotes-sectionTitle":
+                        # role = "Tank"
+                        role = val
+                        print(f"Roles: {role}")
                                     # div class="PatchNotesHeroUpdate"
                                         # div class="PatchNotesHeroUpdate-header"
                                             # h5 class="PatchNotesHeroUpdate-name"
-    hero = "Ramattra"
+                    elif clss == "PatchNotesHeroUpdate-name":
+                        # hero = "Ramattra"
+                        hero = val
+                        print(f"\tCurrent Hero: {hero}")
                                             # div class="PatchNotesHeroUpdate-body"
                                                 # div class="PatchNotesHeroUpdate-abilitiesList"
                                                     # div class="PatchNotesAbilityUpdate"
                                                         # div class="PatchNotesAbilityUpdate-name"
-    ability = "Nemesis Form"
+                    elif clss == "PatchNotesAbilityUpdate-name":
+                        # ability = "Nemesis Form"
+                        ability = val
+                        print(f"\t\tCurrent Ability: {ability}")
                                                         # div class="PatchNotesAbilityUpdate-detailList"
                                                             # ul
                                                                 # li (changes)
-    changes = [
-        "Cooldown increased from 7 to 8 seconds.",
-        "Base armor reduced from 100 to 75.",
-        "Base health reduced from 275 to 250."
-    ]
+                    elif clss == "PatchNotesAbilityUpdate-detailList":
+                        # changes = [
+                        #     "Cooldown increased from 7 to 8 seconds.",
+                        #     "Base armor reduced from 100 to 75.",
+                        #     "Base health reduced from 275 to 250."
+                        # ]
+                        changes = val
+                        print(f"\t\t\tChanges: {changes}")
+                else:
+                    print(res)
+        # else:
+        #     print("Error! No attrs")
 
+from lxml import html
 
-def parse_table(soup: BeautifulSoup) -> pd.DataFrame:
-    """ Scrapes a table from the webpage into a pandas dataframe
+def parse_xpth(url: str):
+    # Perform the request
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+    }
+    response = requests.get(url=url, headers=headers, timeout=10)
 
-    Args:
-        html (BeautifulSoup): HTML file that has been received and parsed by BeautifulSoup
+    # Check if the HTTP request was successful
+    assert response.status_code == 200
 
-    Returns:
-        pd.DataFrame: DataFrame that was constructed and formatted from the table
-    """
-    tbl = soup.find("table")
-    df = pd.read_html(str(tbl))[0]
+    tree: html.HtmlElement = html.fromstring(response.content)
+    elements: List[html.HtmlElement] = tree.xpath("/html/body/main/blz-section/div[@class='PatchNotes-list']/div[@class='PatchNotes-body']/div[@class='PatchNotes-live']")
+    for elem in elements:
+        titles: List[html.HtmlElement] = elem.xpath("h3[@class='PatchNotes-patchTitle']")
+        for title in titles:
+            print(f"Patch: {title.text}")
+            split_title = title.text.split(" - ")
+            name = split_title[0]
+            date = datetime.datetime.strptime(split_title[1], "%B %d, %Y")
+            sections: List[html.HtmlElement] = elem.xpath("div[@class='PatchNotes-section-hero_update']")
+            for section in sections:
+                print(f"\t{section.text}")
+                roles = section.xpath("div/div")
+                for role in roles:
+                    heroes = role.xpath("")
+                    for hero in heroes:
 
-    return df
-
+    # # https://overwatch.blizzard.com/en-us/news/patch-notes/
+    # # html/body/main/blz-section class="PatchNotesBody"/div class="PatchNotes-list"/div class="PatchNotes-body"/div class="PatchNotes-path PatchNotes-live"
+    # # NOTE: This section contains the important info on each patch
+        # # h3 class="PatchNotes-patchTitle"
+        # # div class="PatchNotes-section PatchNotes-section-hero_update"
+            # # h4 class="PatchNotes-sectionTitle"
+            # # div class="PatchNotesHeroUpdate"/div class="PatchNotesHeroUpdate-header"
+                    # # h5 class="PatchNotesHeroUpdate-name"
+                    # # div class="PatchNotesHeroUpdate-body"/div class="PatchNotesHeroUpdate-abilitiesList"/div class="PatchNotesAbilityUpdate"
+                        # # div class="PatchNotesAbilityUpdate-name"
+                        # # div class="PatchNotesAbilityUpdate-detailList"/ul/li (changes)
 
 
 if __name__ == "__main__":
     URL = "https://overwatch.blizzard.com/en-us/news/patch-notes/"
-    HTML = scrape_website(url=URL)
-    db = parse_html(soup=HTML, tag=['script'])
+    # HTML = scrape_website(url=URL)
+    # parse_html(soup=HTML, tag=['script'])
+    ps = parse_xpth(URL)
     # db.to_csv(path_or_buf=f"{RTDIR}/groceries.csv", index=False, header=False)
     # db = parse_table(soup=HTML)
-    print(db)
+    # print(db)
