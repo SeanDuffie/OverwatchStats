84bcfb0123ca3d69efb8e5c1115a3f0c71437618
SeanDuffie <duffiesean@gmail.com>
2024-09-04 17:44:12 -0400

    Marking some points for how the calculator works

--- game_tracker.py
+++ game_tracker.py
@@ -1,1 +1,5 @@
 # TODO: Track leaderboard statistics when pressing tab
+# NOTE: Eliminations / Damage dealt show how often someone you shoot dies.
+#   The better this ratio is, the more effective the damage that you deal is, as you are actually getting elims and not feeding tanks/support
+# NOTE: Eliminations are given when you contribute *UNHEALED* damage to a kill
+# NOTE: Assists are given when you apply a buff/healing to an ally who gets a kill, or a debuff to an enemy who is killed.
