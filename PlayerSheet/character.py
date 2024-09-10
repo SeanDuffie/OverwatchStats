b60dd441d36fd5a1a22ab0504d8d25f99e20ec2c
SeanDuffie <duffiesean@gmail.com>
2024-09-10 16:58:28 -0400

    Made Character abstract Added Doxygen

--- PlayerSheet/character.py
+++ PlayerSheet/character.py
@@ -2,13 +2,23 @@
     @author Sean Duffie
     @brief Contains the stats from a character
 """
+import abc
 import datetime
+from typing import List
 
 from ability import Ability
 from ult import Ult
 
 
-class Character:
+class Character(abc.ABC):
+    """_summary_
+
+    Args:
+        abc (_type_): _description_
+    """
+    a_list: List[Ability]
+    ult: Ult
+
     def __init__(self, name):
         self.name = name
         self.cls = "Support"
@@ -27,10 +37,16 @@ class Character:
         #E
         #Ult
 
+    @abc.abstractmethod
     def fromsite(self):
+        """_summary_
+        """
         pass
 
+    @abc.abstractmethod
     def timeline(self):
+        """_summary_
+        """
         cycle = {
             # ability: [dmg, dur, cool]
             "recon": [360, 4, 8]
@@ -38,18 +54,67 @@ class Character:
         pass
 
     def burst_damage(self, accuracy: float = 0.5, headshots: float = 0.5, splash: int = 1) -> float:
+        """ How much damage can they do over a short period of time?
+
+        FIXME: Should healing and damage be combined for burst/avg? (Return Tuple instead)
+
+        Args:
+            accuracy (float, optional): What percent of shots do you hit?. Defaults to 0.5.
+            headshots (float, optional): What percent of hits are headshots?. Defaults to 0.5.
+            splash (int, optional): How many people are being hit?. Defaults to 1.
+
+        Returns:
+            float: Damage that is output over a short period of time (no reloads/cooldowns)
+        """
         pass
 
     def burst_healing(self, accuracy: float = 0.5, headshots: float = 0.5, splash: int = 1) -> float:
+        """ How much healing can they do over a short period of time?
+
+        FIXME: Should healing and damage be combined for burst/avg? (Return Tuple instead)
+
+        Args:
+            accuracy (float, optional): What percent of shots do you hit?. Defaults to 0.5.
+            headshots (float, optional): What percent of hits are headshots?. Defaults to 0.5.
+            splash (int, optional): How many people are being hit?. Defaults to 1.
+
+        Returns:
+            float: Healing that is output over a short period of time (no reloads/cooldowns)
+        """
         pass
 
     def average_damage(self, accuracy: float = 0.5, headshots: float = 0.5, splash: int = 1) -> float:
+        """ How much damage can they do over a short period of time?
+
+        FIXME: Should healing and damage be combined for burst/avg? (Return Tuple instead)
+
+        Args:
+            accuracy (float, optional): What percent of shots do you hit?. Defaults to 0.5.
+            headshots (float, optional): What percent of hits are headshots?. Defaults to 0.5.
+            splash (int, optional): How many people are being hit?. Defaults to 1.
+
+        Returns:
+            float: Damage that is output over a short period of time (no reloads/cooldowns)
+        """
         pass
 
     def average_healing(self, accuracy: float = 0.5, headshots: float = 0.5, splash: int = 1) -> float:
+        """ How much damage can they do over a short period of time?
+
+        FIXME: Should healing and damage be combined for burst/avg? (Return Tuple instead)
+
+        Args:
+            accuracy (float, optional): What percent of shots do you hit?. Defaults to 0.5.
+            headshots (float, optional): What percent of hits are headshots?. Defaults to 0.5.
+            splash (int, optional): How many people are being hit?. Defaults to 1.
+
+        Returns:
+            float: Damage that is output over a short period of time (no reloads/cooldowns)
+        """
         pass
 
     def ult_time(self) -> datetime.timedelta:
+        pass
 
 if __name__ == "__main__":
     pass
